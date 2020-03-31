d07e7d19d43e015d002212602b08886a9b055b71
eiire <bulax.d.2101@gmail.com>
2020-03-31 18:16:24 +0700

    "JSON_realization_parsing(conclusion of a tree and fix functions)#checking_nesting not work"

--- src/compiler.py
+++ src/compiler.py
@@ -1,11 +1,13 @@
 import sys
 from src.lexer import *
 from src.parser import *
+import json
+from bs4 import BeautifulSoup
 
 
 def start_compiler():
     file = open("input.txt", 'r')
-    file_res = open("result.txt", 'w')
+    file_res = open("../build/result.txt", 'w')
 
     if len(sys.argv) == 2:
         if sys.argv[1] == "--dump-tokens":
@@ -16,35 +18,39 @@ def start_compiler():
                     # Generator_tokens --> lexer.tokens_in_line
                     # slize for thist # beginning left
                     for tok in tokens_in_line(line[0:check_thist_heshteg_after_cov(line) - 1], line_number):
-                        print(tok.position, " \t", tok.name, "#", tok.token)
+                        file_res.write(tok.position + " \t" + tok.lexeme + ' ' + "#" + tok.token_type + '\n')
                 else:
                     # slize for thist # beginning left
                     for tok in tokens_in_line(line.partition('#')[0], line_number):
-                        print(tok.position, "\t", tok.name, "#", tok.token)
+                        file_res.write(tok.position + "\t" + tok.lexeme + ' ' + "#" + tok.token_type + '\n')
 
+        # исправить баг со строкой
         elif sys.argv[1] == "--dump-ast":
             line_number = 0
             for line in file:
                 line_number += 1
-                if line.find('"') < line.find('#') < line.rfind('"'):
+                if line.find('"') < line.find('#') < line.rfind('"') and line != '\n':
                     try:
                         iterator = iter(tokens_in_line(line[0:check_thist_heshteg_after_cov(line) - 1], line_number))
                         next(iterator)
                         for tok in tokens_in_line(line[0:check_thist_heshteg_after_cov(line) - 1], line_number):
                             next_tok = next(iterator)
-                            parsing(tok, next_tok)
+                            if parsing(tok, next_tok) != 'Next': return print('ERROR in: ', tok.position)
                     except StopIteration:
+                        # пустая строка, вызов парсера с предыдущими значениями токена
                         none_tok = Token('None', 'None', 'end_str')
-                        parsing(tok, none_tok)
-                else:
+                        if parsing(tok, none_tok) != 'Next': return print('ERROR in: ', tok.position)
+
+                elif (line.find('"') < line.find('#') < line.rfind('"')) == False and line != '\n':
                     try:
                         iterator = iter(tokens_in_line(line.partition('#')[0], line_number))
                         next(iterator)
                         for tok in tokens_in_line(line.partition('#')[0], line_number):
                             next_tok = next(iterator)
                             if parsing(tok, next_tok) != 'Next': return print('ERROR in: ', tok.position)
+
                     except StopIteration:
-                        # print("F")
+                        # пустая строка, вызов парсера с предыдущими значениями токена
                         none_tok = Token('None', 'None', 'end_str')
                         if parsing(tok, none_tok) != 'Next': return print('ERROR in: ', tok.position)
 
@@ -59,5 +65,6 @@ def start_compiler():
 
 
 start_compiler()
-print(ast)
-print(stack_nodes_hierarchy)
+
+with open("../build/AST.json", "w", encoding="utf-8") as file:
+    json.dump(ast, file, indent=4)
--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,5 @@
 from src.lexer import Token
 
-
 parser_table = {
     'keyword_for': ['l_paren'],
     'identifier_variable': ['operator_assignment', 'operator_sum', 'operator_grater', 'operator_less', 'r_paren', 'semi'],
@@ -9,12 +8,11 @@ parser_table = {
     'keyword_if': ['l_paren'],
     'identifier': ['operator_assignment', 'semi', 'operator_sum', 'identifier_variable'],
     'semi': ['None', 'keyword_for', 'semi', 'r_paren', 'numeric_constant', 'identifier_variable'],
-    # if str end --> None, else --> next_tok may be same tok(example 'for')
     'numeric_constant': ['semi', 'r_paren', 'l_brace'],
     'keyword_<?php': ['None'],
     'operator_assignment': ['numeric_constant', 'identifier_variable'],
     'r_paren': ['semi', 'l_brace', 'r_brace'],
-    'l_brace': ['None', 'identifier'],
+    'l_brace': ['None', 'identifier', 'identifier_variable'],
     'operator_grater': ['numeric_constant'],
     'keyword_break': ['semi'],
     'r_brace': ['None'],
@@ -33,11 +31,6 @@ constructions = {'keyword_for': [['identifier_variable', 'operator_assignment', 
                                 'numeric_constant']
                  }
 
-
-stack_nodes_hierarchy = ['program']
-keyword_construction = Token('None', 'None', 0)  # расширить стеком
-
-
 ast = {
     "kind": "program",
     "children": [
@@ -45,122 +38,132 @@ ast = {
     ]
 }
 
+stack_nodes_hierarchy = ['program']
+keyword_construction = Token('None', 'None', 0)  # расширить стеком
 
-def node_creating(token):
+
+# ----->MAIN FUNC for AST<------
+def node_creating(current_token, next_token, keyword_construction):
     need_lvl = ast
-    stack = stack_nodes_hierarchy
-    print(need_lvl)
-    print(stack)
-    for construction in stack_nodes_hierarchy: # ----->MAIN FUNC<------
-        # print(construction)
+
+    for construction in stack_nodes_hierarchy:
         for el_on_lvl in reversed(need_lvl["children"]):
             if el_on_lvl["kind"] == construction: #  BODY
                 need_lvl = el_on_lvl
                 break
 
     # Describe all constructions
-    if token.token_type == 'keyword_for':
-        need_lvl["children"].append({"kind": token.token_type,
-                                     "init": "",
-                                     "test": "",
-                                     "increment": "",
-                                     "position": token.position,
-                                     "children": []
-                                     })
+    if keyword_construction.token_type == 'keyword_for':
+        if current_token.token_type == 'keyword_for' and keyword_construction.token_type == 'keyword_for' \
+                and current_token.lexeme != '{' and current_token.lexeme != '}':
+            need_lvl["children"].append({"kind": current_token.token_type,
+                                         "init": "",
+                                         "test": "",
+                                         "increment": "",
+                                         "position": current_token.position,
+                                         "children": []
+                                         })
+        elif keyword_construction.token_type == 'keyword_for' and keyword_construction.position == 0:
+            if current_token.lexeme != '(' and current_token.lexeme != ';' and current_token.lexeme != ')':
+                need_lvl["init"] = need_lvl["init"] + current_token.lexeme + ' '
+        elif keyword_construction.token_type == 'keyword_for' and keyword_construction.position == 1:
+            if current_token.lexeme != '(' and current_token.lexeme != ';' and current_token.lexeme != ')':
+                need_lvl["test"] = need_lvl["test"] + current_token.lexeme + ' '
+        elif keyword_construction.token_type == 'keyword_for' and keyword_construction.position == 2:
+            if current_token.lexeme != '(' and current_token.lexeme != ';' and current_token.lexeme != ')':
+                need_lvl["increment"] = need_lvl["increment"] + current_token.lexeme + ' '
 
-    if token.token_type == 'exp':
-        need_lvl["children"].append({"kind": token.token_type,
-                                     "position": token.position,
+    if keyword_construction.token_type == 'assign' and keyword_construction.position == 0 \
+            and current_token.lexeme != '{' and current_token.lexeme != '}':
+        need_lvl["children"].append({"kind": keyword_construction.token_type,
+                                     "left": current_token.lexeme,
+                                     "right": "",
+                                     "position": keyword_construction.position,
                                      })
+    elif keyword_construction.token_type == 'assign' and keyword_construction.position == 1 \
+            and current_token.lexeme != '=':
+        need_lvl["children"][-1]["right"] = need_lvl["children"][-1]["right"] + current_token.lexeme + ' '
 
 
 current_lvl = ast
 
-#
-# def search_lvl():
-#     lvl = 0
-#     for el in nesting_stack:
-#         if el == '{':
-#             lvl += 1
-#         if el == '}':
-#             lvl -= 1
-#     return lvl
-
 
 def parsing(current_tok, next_tok):
-    global ast
-    global prev_lvl
-    str_exp = ""
-    if next_tok.token_type in parser_table[current_tok.token_type]:
-        #block_php
-        if current_tok.position == "0:0" and current_tok.token_type == 'keyword_<?php':
+    # print(current_tok.lexeme, next_tok.lexeme)
+    try:
+        if next_tok.token_type in parser_table[current_tok.token_type]:
+            #block_php
+            if current_tok.position == "1:0" and current_tok.token_type == 'keyword_<?php':
+                return 'Next'
+            elif current_tok.position != "1:0" and current_tok.token_type == 'keyword_<?php':
+                return current_tok.position
+
+            check_for(current_tok, next_tok)
+
+            check_instruction(current_tok, next_tok)
+
+            #  exit from namespace
+            if current_tok.lexeme == '}':
+                stack_nodes_hierarchy.pop()
+
+            if current_tok.lexeme == '{' or current_tok.lexeme == '}':
+                nesting_stack.append(current_tok)
+                check_nesting()
             return 'Next'
-        #endblock
+        else:
+            return current_tok.position
+    except KeyError:
+        print("Non-existent: ", current_tok.lexeme, current_tok.position)
 
-        #block for
-        if current_tok.token_type == 'keyword_for' or keyword_construction.token_type == 'keyword_for':
-            # prev_lvl = search_lvl(stack_nodes_hierarchy, ast)
-            node_creating(current_tok)
 
-            keyword_construction.token_type = 'keyword_for'
-            if current_tok.token_type == 'keyword_for':
-                stack_nodes_hierarchy.append(keyword_construction.token_type)  # LVL
+# general case (если конструкция не определена, значит конструкция - инструкция)
+def check_instruction(current_tok, next_tok):
+    if keyword_construction.token_type == 'None' or keyword_construction.token_type == 'assign':
+        keyword_construction.token_type = 'assign'
 
-            if current_tok.lexeme == ';':
-                keyword_construction.position += 1
-                if (keyword_construction.position == 2 or 1 or 0) and next_tok.position == 'end_str':
-                    return current_tok.position
-                # tree insert
+        # left and right part instruction
+        if current_tok.lexeme == '=' or current_tok.lexeme == '<' or current_tok.lexeme == '>':
+            keyword_construction.position += 1
 
-            if keyword_construction.position != 2 and check_construction(current_tok, next_tok, keyword_construction) \
-                    == 'End_construction':
+        if current_tok.token_type == 'semi':
+            keyword_construction.token_type = 'None'
+            keyword_construction.position = 0
+
+        node_creating(current_tok, next_tok, keyword_construction)
+
+
+def check_for(current_tok, next_tok):
+    if current_tok.token_type == 'keyword_for' or keyword_construction.token_type == 'keyword_for':
+        keyword_construction.token_type = 'keyword_for'
+
+        if current_tok.token_type == 'keyword_for':
+            stack_nodes_hierarchy.append(keyword_construction.token_type)  # LVL
+
+        if current_tok.lexeme == ';':
+            keyword_construction.position += 1
+            if (keyword_construction.position == 2 or 1 or 0) and next_tok.position == 'end_str':
                 return current_tok.position
+            # tree insert
 
-            if current_tok.token_type in constructions[keyword_construction.token_type][keyword_construction.position] \
-                    == False:
-                return current_tok.position
+        if keyword_construction.position != 2 and check_construction(current_tok, next_tok, keyword_construction) \
+                == 'End_construction':
+            return current_tok.position
 
-            if check_construction(current_tok, next_tok, keyword_construction) == -1:
-                return current_tok.position
-            elif check_construction(current_tok, next_tok, keyword_construction) == 'End_construction':
-                keyword_construction.token_type = 'None'
-                keyword_construction.position = 0
+        # if current_tok.token_type in constructions[keyword_construction.token_type][keyword_construction.position]:
+        #     print(current_tok.lexeme, next_tok.lexeme)
+        #     # return current_tok.position
 
-        #block exp
-        # print(current_tok.lexeme, next_tok.lexeme)
-        if current_tok.token_type == 'identifier_variable' and next_tok.token_type == 'operator_assignment' and \
-                keyword_construction.token_type != 'keyword_for':
-            keyword_construction.token_type = 'exp'
-            node_creating(keyword_construction)
+        if check_construction(current_tok, next_tok, keyword_construction) == -1:
+            return current_tok.position
+        elif check_construction(current_tok, next_tok, keyword_construction) == 'End_construction':
+            keyword_construction.token_type = 'None'
+            keyword_construction.position = 0
 
-            if current_tok.token_type == 'semi':
-                keyword_construction.token_type = 'None'
-        #endblock
+        node_creating(current_tok, next_tok, keyword_construction)
 
-        if current_tok.lexeme == '}':
-            stack_nodes_hierarchy.pop()
-        #endfor
+        return 1
 
-        # if current_tok.token_type == 'keyword_if' or keyword_construction.token_type == 'keyword_if':
-        #     keyword_construction.token_type = 'keyword_if'
-        #     if check_construction(current_tok, next_tok, keyword_construction) == -1:
-        #         return current_tok.position
-        #     elif check_construction(current_tok, next_tok, keyword_construction) == 'End_construction':
-        #         # print(keyword_construction.token_type, keyword_construction.position)
-        #         keyword_construction.token_type = 'None'
-        #         keyword_construction.position = 0
-
-
-
-        if current_tok.lexeme == '(' or current_tok.lexeme == ')' \
-                or current_tok.lexeme == '{' or current_tok.lexeme == '}':
-            nesting_stack.append(current_tok.lexeme)
-            print(nesting_stack) # <---
-            check_nesting()
-        return 'Next'
-    else:
-        print(current_tok.lexeme)
-        return current_tok.position
+    return 0
 
 
 def check_construction(curent_tok,  next_tok, keyword_construction):
@@ -175,27 +178,8 @@ count_brace = 0
 
 
 def check_nesting():
-    global count_paren
-    global count_brace
-    for i in range(len(nesting_stack)):
-        if nesting_stack[i] == '(':
-            count_paren += 1
-
-        if nesting_stack[i] == '{':
-            count_brace += 1
-            if count_paren != 0: return -1
-
-        if nesting_stack[i] == ')':
-            count_paren -= 1
-            if count_paren == -1: return -1
-
-        if nesting_stack[i] == '}':
-            count_brace -= 1
-            if count_brace == -1: return -1
-
-
-def build_parse_tree():
     pass
+
 
 # ['(']
 # ['(', ')']
@@ -205,3 +189,14 @@ def build_parse_tree():
 # ['(', ')', '{', '(', ')', '{']
 # ['(', ')', '{', '(', ')', '{', '}']
 # ['(', ')', '{', '(', ')', '{', '}', '}']
+
+#block if
+# if current_tok.token_type == 'keyword_if' or keyword_construction.token_type == 'keyword_if':
+#     keyword_construction.token_type = 'keyword_if'
+#     if check_construction(current_tok, next_tok, keyword_construction) == -1:
+#         return current_tok.position
+#     elif check_construction(current_tok, next_tok, keyword_construction) == 'End_construction':
+#         # print(keyword_construction.token_type, keyword_construction.position)
+#         keyword_construction.token_type = 'None'
+#         keyword_construction.position = 0
+#endif
