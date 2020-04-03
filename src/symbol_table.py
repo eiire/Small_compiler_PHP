a9f63524660be61a63d6e95358fd430c5887c77c
eiire <bulax.d.2101@gmail.com>
2020-04-03 15:33:30 +0700

    "Проверка AST-tree и symbol_table в res_labs2&3"

--- src/symbol_table.py
+++ src/symbol_table.py
@@ -43,38 +43,3 @@ def craft_symbol_table(current_lvl, current_token):
         # Создание областей
         if current_token.lexeme == '{':
             counter_ns.update({current_lvl: ['{']})
-
-# symbol_table = {
-#     '0:0': [],
-# }
-# counter_ns = {
-#     "0": []
-# }
-# def craft_symbol_table(current_lvl, current_token, stack_node_hierarchy):
-#     global counter_ns
-#     lvls = [lvl for lvl in list(symbol_table.keys())]
-#
-#
-#     if current_lvl in lvls:
-#         if current_token.lexeme == '{':
-#             counter_ns[current_lvl].append('{')
-#
-#         symbol_table[current_lvl +':'+ str(len(counter_ns[current_lvl]))].append(current_token.lexeme)
-#
-#     elif current_lvl not in lvls:
-#         if current_token.lexeme == '{':
-#             counter_ns.update({current_lvl: ['{']})
-#
-#         if check_for_identifier(current_token):
-#             symbol_table.update({current_lvl +':' + str(len(counter_ns[current_lvl])): [current_token.lexeme]})
-#     print(counter_ns)
-
-# lvls = [lvl for lvl in list(symbol_table.keys())]
-#
-# if current_lvl in lvls:
-#     if check_for_identifier(current_token) and current_token.lexeme not in symbol_table[current_lvl]:
-#         symbol_table[current_lvl].append(current_token.lexeme)
-#
-# elif current_lvl not in lvls:
-#     if check_for_identifier(current_token):
-#         symbol_table.update({current_lvl: [current_token.lexeme]})
